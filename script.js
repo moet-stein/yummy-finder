1bcec7c9460590deca437a8121335bc845e00cc4
Moe Steinmueller <0101.mste@gmail.com>
2021-04-13 17:03:44 +0200

    added show more and less on home page

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// SHOW AND HIDE (ABOUT SECTION)
+const showHide = (downArrow, moreText) => {
+  let pAndIcon = downArrow.childNodes;
+  let moreOrLess = pAndIcon[0];
+  let arrowIcon = pAndIcon[2];
+  if (moreText.classList.contains('hide')) {
+    moreText.classList.remove('hide');
+    moreOrLess.innerHTML = 'Show Less';
+    arrowIcon.innerHTML = 'expand_less';
+  } else {
+    moreText.classList.add('hide');
+    moreOrLess.innerHTML = 'Show More';
+    arrowIcon.innerHTML = 'expand_more';
+  }
+};
+// ABOUT
+let downArrow1 = document.getElementById('downArrow1');
+const moreText1 = document.getElementById('moreText1');
+downArrow1.addEventListener('click', () => showHide(downArrow1, moreText1));
+// Search recipes
+let downArrow2 = document.getElementById('downArrow2');
+const moreText2 = document.getElementById('moreText2');
+downArrow2.addEventListener('click', () => showHide(downArrow2, moreText2));
+// Save recipes
+let downArrow3 = document.getElementById('downArrow3');
+const moreText3 = document.getElementById('moreText3');
+downArrow3.addEventListener('click', () => showHide(downArrow3, moreText3));
+// Search Cooking Videos
+let downArrow4 = document.getElementById('downArrow4');
+const moreText4 = document.getElementById('moreText4');
+downArrow4.addEventListener('click', () => showHide(downArrow4, moreText4));
+
 var options = {
   edge: 'left',
 };
