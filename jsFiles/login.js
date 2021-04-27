888d0fbcfae7510708f978f8aee763829d3554f2
Moe Steinmueller <0101.mste@gmail.com>
2021-04-27 14:37:03 +0200

    showing profile img when logged in

--- jsFiles/login.js
+++ jsFiles/login.js
@@ -7,6 +7,8 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 // Login(sign in)
 const loginFunc = () => {
   const loginBtn = document.getElementById('login');
+  const errorP = document.getElementById('errorP');
+
   loginBtn.addEventListener('click', (event) => {
     event.preventDefault();
     const emailInputValue = document.getElementById('email').value;
@@ -23,6 +25,7 @@ const loginFunc = () => {
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorCode, errorMessage);
+        errorP.innerHTML = errorMessage;
       });
   });
   // Add a real time listener
@@ -33,6 +36,7 @@ const loginFunc = () => {
       // var uid = user.uid;
       console.log(user);
       btnLogout.classList.remove('hide');
+
       if (document.title == 'YummyFinder-Login') {
         window.location = '../index.html';
       }
@@ -48,8 +52,15 @@ const logoutFunc = () => {
   const btnLogoutSide = document.getElementById('btnLogoutSide');
   const btnLogin = document.getElementById('btnLogin');
   const btnLoginSide = document.getElementById('btnLoginSide');
+  const acountImage = document.getElementById('acountImage');
+  const sideAcountImage = document.getElementById('sideAcountImage');
+  const sideImg = document.getElementById('sideImg');
 
   btnLogout.addEventListener('click', (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  });
+  btnLogoutSide.addEventListener('click', (e) => {
     e.preventDefault();
     firebase.auth().signOut();
   });
@@ -57,18 +68,31 @@ const logoutFunc = () => {
   // Add a real time listener
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+      firebase
+        .storage()
+        .ref(`users/${user.uid}/profile.jpg`)
+        .getDownloadURL()
+        .then((imgUrl) => {
+          console.log('i am supposed to show image');
+          acountImage.setAttribute('src', imgUrl);
+          sideImg.setAttribute('src', imgUrl);
+        });
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
       // var uid = user.uid;
       console.log(user);
       btnLogout.classList.remove('hide');
       btnLogoutSide.classList.remove('hide');
+      acountImage.classList.remove('hide');
+      sideAcountImage.classList.remove('hide');
       btnLogin.classList.add('hide');
       btnLoginSide.classList.add('hide');
     } else {
       console.log('not logged in');
       btnLogout.classList.add('hide');
       btnLogoutSide.classList.add('hide');
+      acountImage.classList.add('hide');
+      sideAcountImage.classList.add('hide');
       btnLogin.classList.remove('hide');
       btnLoginSide.classList.remove('hide');
       if (document.title == 'YummyFinder-Saved') {
